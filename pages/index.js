3c7a6324ce9bec5998ad15a9400829229f5374e8
Trevor <tr89on@gmail.com>
2022-05-18 16:24:18 -0700

    add stuff for guessing game

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,49 @@ import styles from '../styles/Home.module.css'
 import {useState, useEffect} from 'react'
 
 export default function Home() {
-
+  const [revealed, setRevealed] = useState([]);
   const [topSongs, setTopSongs] = useState([]);
   const [curGuess, setCurGuess] = useState(0);
 
-  const showTopSongs = () => {
-    return topSongs.map(song => 
-      <li key={song}>{song}</li>  
-    )
+  const reset = () => {
+    setRevealed([]);
+    setTopSongs([]);
+    setCurGuess(0);
   }
+
+  const initRandomData = () => {
+    const randomData = ['blah', 'trevrawr'];
+    const newRevealed = [];
+    const newTopSongs = [];
+
+    randomData.forEach(item => {
+        newRevealed.push(false);
+        newTopSongs.push(item);
+      }
+    )
+    setRevealed(newRevealed);
+    setTopSongs(newTopSongs);
+  }
+
+  const generateGuesses = () => {
+    const answer = topSongs[curGuess];
+
+    
+
+  }
+
+  const reveal = () => {
+    const newRevealed = [...revealed]
+    newRevealed[curGuess] = true;
+
+    setRevealed(newRevealed);
+    setCurGuess(curGuess + 1);
+  }
+
+  const showTopSongs = topSongs.map((song, idx) => 
+    revealed[idx] ? <li key={idx}>{song}</li> : <li key={idx}>???</li>
+  );
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -23,7 +57,7 @@ export default function Home() {
         <div>{curGuess}</div>
         <button onClick={() => setCurGuess(curGuess+1)}>click me</button>
 
-        <button onClick={() => setTopSongs(['blah','trevrawr'])}>click me too!</button>
+        <button onClick={initRandomData}>click me too!</button>
 
         <div className={`${styles.card} ${styles.btn}`}>
           <h2>Login with Spotify</h2>
@@ -45,9 +79,12 @@ export default function Home() {
           <div className={`${styles.card} ${styles.btn}`}>
             <h2>All Time</h2>
           </div>
+        </div>
 
-          <div className={`${styles.card} ${styles.btn}`}>
-            <h2>Deploy &rarr;</h2>
+        <div className={styles.grid}>
+
+          <div onClick={reveal} className={`${styles.card} ${styles.btn}`}>
+            <h2>Guess</h2>
           </div>
         </div>
       </main>

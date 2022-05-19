91ff431c0a50e5dc698f85cb19dca6269fcaac59
Trevor Wong <trevorwongalt@gmail.com>
2022-05-18 18:23:53 -0700

    Game logic implemented

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,27 @@ import styles from '../styles/Home.module.css'
 import {useState, useEffect} from 'react'
 
 export default function Home() {
+
+  const NUMSONGS = 50
+
   const [revealed, setRevealed] = useState([]);
   const [topSongs, setTopSongs] = useState([]);
   const [curGuess, setCurGuess] = useState(0);
+  const [curSongs, setCurSongs] = useState([]);
+
+  const getRandomInt = (min, max) => {min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)}
 
   const reset = () => {
     setRevealed([]);
     setTopSongs([]);
     setCurGuess(0);
+    setCurSongs([]);
   }
 
   const initRandomData = () => {
-    const randomData = ['blah', 'trevrawr'];
+    const randomData = ['blah', 'trevrawr', 'minecraft', 'joseph', 'cube', 'hello'];
     const newRevealed = [];
     const newTopSongs = [];
 
@@ -24,6 +33,7 @@ export default function Home() {
         newTopSongs.push(item);
       }
     )
+    
     setRevealed(newRevealed);
     setTopSongs(newTopSongs);
   }
@@ -46,19 +56,37 @@ export default function Home() {
     setTopSongs(data.items)
   }
 
-  const generateGuesses = () => {
-    const answer = topSongs[curGuess];
-
+  const generateGuesses = (curGuessNum) => {
+    const tempSongs = []
+    const answerIdx = getRandomInt(0,4);
     
+    for (let i = 0; i <4; i++){
+      if(i === answerIdx){
+        tempSongs.push(topSongs[curGuessNum])
+      }
+      else{
+        const randomSong = topSongs[getRandomInt(curGuessNum+1,topSongs.length)];
+        tempSongs.push(randomSong)
+      }
+    }
 
+    console.log(curGuessNum)
+    console.log(tempSongs)
+    console.log(topSongs[curGuessNum])
+    
+    setCurSongs(tempSongs)
   }
 
-  const reveal = () => {
-    const newRevealed = [...revealed]
-    newRevealed[curGuess] = true;
-
-    setRevealed(newRevealed);
-    setCurGuess(curGuess + 1);
+  const reveal = (guess) => {
+    if(guess === topSongs[curGuess]){
+      const newRevealed = [...revealed]
+      newRevealed[curGuess] = true;
+      
+      setRevealed(newRevealed);
+      setCurGuess(curGuess + 1);
+      generateGuesses(curGuess + 1)
+    }
+    
   }
 
   const showTopSongs = topSongs.map((song, idx) => 
@@ -73,39 +101,57 @@ export default function Home() {
         </h1>
 
         <div>{curGuess}</div>
-        <button onClick={() => setCurGuess(curGuess+1)}>click me</button>
-
         <button onClick={initRandomData}>click me too!</button>
+        <button onClick={() => generateGuesses(curGuess)}>Generate Guesses</button>
 
         <div className={`${styles.card} ${styles.btn}`}>
           <h2>Login with Spotify</h2>
         </div>
 
-        <list>
-          {showTopSongs}
-        </list>
-
         <div className={styles.grid}>
-          <div className={`${styles.card} ${styles.btn}`} onClick={() => getTopSongs4Weeks()}>
+          <div className={`${styles.card} ${styles.btn}`} onClick={() => {getTopSongs4Weeks(); generateGuesses();} }>
             <h2>Last 4 Weeks</h2>
 
           </div>
 
-          <div className={`${styles.card} ${styles.btn}`} onClick={() => getTopSongs6Months()}>
+          <div className={`${styles.card} ${styles.btn}`} onClick={() => {getTopSongs6Months(); generateGuesses();} }>
             <h2>Last 6 months</h2>
           </div>
 
-          <div className={`${styles.card} ${styles.btn}`} onClick={() => getTopSongsAllTime()}>
+          <div className={`${styles.card} ${styles.btn}`} onClick={() => {getTopSongsAllTime(); generateGuesses();} }>
             <h2>All Time</h2>
           </div>
         </div>
+        
+        {/* Display songs */}
+        { topSongs.length > 0 &&
+        <>
+        <list>
+          {showTopSongs}
+          {topSongs}
+        </list>
 
+        <h1>Choose Your Next Top Song</h1>
         <div className={styles.grid}>
+          
+          <div onClick={() => reveal(curSongs[0])} className={`${styles.card} ${styles.btn}`}>
+            <h2>{curSongs[0]}</h2>
+          </div>
 
-          <div onClick={reveal} className={`${styles.card} ${styles.btn}`}>
-            <h2>Guess</h2>
+          <div onClick={() => reveal(curSongs[1])} className={`${styles.card} ${styles.btn}`}>
+            <h2>{curSongs[1]}</h2>
+          </div>
+
+          <div onClick={() => reveal(curSongs[2])} className={`${styles.card} ${styles.btn}`}>
+            <h2>{curSongs[2]}</h2>
+          </div>
+
+          <div onClick={() => reveal(curSongs[3])} className={`${styles.card} ${styles.btn}`}>
+            <h2>{curSongs[3]}</h2>
           </div>
         </div>
+        </>
+        }
       </main>
 
       <footer className={styles.footer}>
